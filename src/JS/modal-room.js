64789f2c930f43525b24dccd01cdf27d73ed37d1
NahornyiDmytro <119012718+NahornyiDmytro@users.noreply.github.com>
2023-02-01 22:11:35 +0100

    підключення модалки бронювання кімнати
    Створив новий файл в js папці під назвою modal-room та підключив її в index.js.В hero hotel та hero main додав клас для відкриття модалки бронювання кімнати.В хотел та рум хотел scss підключив стилі модалки рум.Ну і прописав код html і scss модалки своєї в reservation-number html + scss.

---
+++ src/JS/modal-room.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-room]"),
+      closeModalBtn: document.querySelector("[data-modal-close-room]"),
+      modal: document.querySelector("[data-modal-room]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
