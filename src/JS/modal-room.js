8f5b88922e03a73e6904cf4fa52bcc955023cec2
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-02-03 15:59:36 +0200

    modal window fix

--- src/JS/modal-room.js
+++ src/JS/modal-room.js
@@ -1,14 +1,18 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open-room]"),
-      closeModalBtn: document.querySelector("[data-modal-close-room]"),
-      modal: document.querySelector("[data-modal-room]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtns: document.querySelectorAll('[data-modal-open-room]'),
+    closeModalBtns: document.querySelectorAll('[data-modal-close-room]'),
+    modal: document.querySelector('[data-modal-room]'),
+  };
+
+  refs.openModalBtns.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtns.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,17 +1,21 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-table]"),
-    closeModalBtn: document.querySelector("[data-modal-close-table]"),
-    modal: document.querySelector("[data-modal-table]"),
+    openModalBtns: document.querySelectorAll('[data-modal-open-table]'),
+    closeModalBtns: document.querySelectorAll('[data-modal-close-table]'),
+    modal: document.querySelector('[data-modal-table]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtns.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtns.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    
-   // openMenuBtn.addEventListener('click', toggleMenu);
- // closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.modal.classList.toggle('is-hidden');
+
+    // openMenuBtn.addEventListener('click', toggleMenu);
+    // closeMenuBtn.addEventListener('click', toggleMenu);
   }
 })();
