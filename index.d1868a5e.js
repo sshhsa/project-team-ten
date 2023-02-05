f67b50d9910dd8f4a81db5714049deb3fd467f18
sshhsa <118224486+sshhsa@users.noreply.github.com>
2023-02-05 18:09:15 +0000

    Deploying to gh-pages from @ sshhsa/project-team-ten@56146f9fda147bc8a296e2e39adc49ed4c7213b5 🚀

---
+++ index.d1868a5e.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtns:document.querySelectorAll("[data-modal-open-table]"),closeModalBtns:document.querySelectorAll("[data-modal-close-table]"),modal:document.querySelector("[data-modal-table]")};function l(){e.modal.classList.toggle("is-hidden")}e.openModalBtns.forEach((e=>{e.addEventListener("click",l)})),e.closeModalBtns.forEach((e=>{e.addEventListener("click",l)}))})();
+//# sourceMappingURL=index.d1868a5e.js.map
---
+++ index.e1d7c339.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtns:document.querySelectorAll("[data-modal-open-table]"),closeModalBtns:document.querySelectorAll("[data-modal-close-table]"),modal:document.querySelector("[data-modal-table]")};function l(){e.modal.classList.toggle("is-hidden")}e.openModalBtns.forEach((e=>{e.addEventListener("click",l)})),e.closeModalBtns.forEach((e=>{e.addEventListener("click",l)}))})();
+//# sourceMappingURL=index.e1d7c339.js.map
